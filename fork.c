37c2b6e86208907b97b0471da98d4b9d74b4dc31
JessOdette Guizado <A01643724@tec.mx>
2023-10-14 06:04:21 +0000

    update

--- fork.c
+++ fork.c
@@ -1,2 +1,13 @@
 #include <stdio.h>
-printf("holas")
+
+int main(){ 
+    printf("soy el proceso padre \n");
+    int pid=fork();
+    if(pid==0){
+        printf("soy el proceso hijo\n");
+    }else{
+        printf("soy el proceso padre y mi hijo es: %d\n",pid);
+    }
+
+    return 0;
+}
